1f4bc5a424ccac233ec58c44e9cbc8d108829acf
mariefherv <mariefherv@gmail.com>
2023-10-16 19:36:25 +0800

    1.0.0
    Added price-table-1

---
+++ public/pricing-table-1/index.js
@@ -0,0 +1,246 @@
+
+(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35730/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
+(function () {
+  'use strict';
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+  function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor);
+    }
+  }
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    Object.defineProperty(Constructor, "prototype", {
+      writable: false
+    });
+    return Constructor;
+  }
+  function _defineProperty(obj, key, value) {
+    key = _toPropertyKey(key);
+    if (key in obj) {
+      Object.defineProperty(obj, key, {
+        value: value,
+        enumerable: true,
+        configurable: true,
+        writable: true
+      });
+    } else {
+      obj[key] = value;
+    }
+    return obj;
+  }
+  function _toPrimitive(input, hint) {
+    if (typeof input !== "object" || input === null) return input;
+    var prim = input[Symbol.toPrimitive];
+    if (prim !== undefined) {
+      var res = prim.call(input, hint || "default");
+      if (typeof res !== "object") return res;
+      throw new TypeError("@@toPrimitive must return a primitive value.");
+    }
+    return (hint === "string" ? String : Number)(input);
+  }
+  function _toPropertyKey(arg) {
+    var key = _toPrimitive(arg, "string");
+    return typeof key === "symbol" ? key : String(key);
+  }
+
+  /**
+   * @name initHeader
+   * @description Initiates Kenzap Cloud extension header and related scripts. Verifies user sessions, handles SSO, does cloud space navigation, initializes i18n localization. 
+   * @param {object} response
+   */
+
+  /*
+   * Translates string based on preloaded i18n locale values.
+   * 
+   * @param text {String} text to translate
+   * @param cb {Function} callback function to escape text variable
+   * @param p {String} list of parameters, to be replaced with %1$, %2$..
+   * @returns {String} - text
+   */
+  const __esc = (text, cb, ...p) => {
+
+      let match = (input, pa) => {
+
+          pa.forEach((p, i) => { input = input.replace('%'+(i+1)+'$', p); }); 
+          
+          return input;
+      };
+
+      if(typeof window.i18n === 'undefined') return match(text, p);
+      if(window.i18n.state.locale.values[text] === undefined) return match(text, p);
+
+      return match(cb(window.i18n.state.locale.values[text]), p);
+  };
+
+  /*
+   * Converts special characters `&`, `<`, `>`, `"`, `'` to HTML entities.
+   * 
+   * @param text {String}  text
+   * @returns {String} - text
+   */
+  const attr = (text) => {
+
+      text = String(text);
+
+      if(text.length === 0){
+  		return '';
+  	}
+
+      return text.replace(/[&<>'"]/g, tag => (
+          {
+              '&': '&amp;',
+              '<': '&lt;',
+              '>': '&gt;',
+              "'": '&apos;',
+              '"': '&quot;'
+          } [tag]));
+  };
+
+  /*
+   * Converts special characters `&`, `<`, `>`, `"`, `'` to HTML entities and does translations
+   * 
+   * @param text {String}  text
+   * @returns {String} - text
+   */
+  const __html = (text, ...p) => {
+
+      text = String(text);
+
+      if(text.length === 0){
+  		return '';
+  	}
+
+      let cb = (text) => {
+
+          return text.replace(/[&<>'"]/g, tag => (
+              {
+                  '&': '&amp;',
+                  '<': '&lt;',
+                  '>': '&gt;',
+                  "'": '&apos;',
+                  '"': '&quot;'
+              } [tag]));
+      };
+
+      return __esc(text, cb, ...p);
+  };
+
+  /**
+   * @name spaceID
+   * @description Gets current Kenzap Cloud space ID identifier from the URL.
+   * 
+   * @returns {string} id - Kenzap Cloud space ID.
+   */
+   const spaceID = () => {
+      
+      let urlParams = new URLSearchParams(window.location.search);
+      let id = urlParams.get('sid') ? urlParams.get('sid') : "";
+
+      return id;
+  };
+
+  /**
+   * @name setCookie
+   * @description Set cookie by its name to all .kenzap.cloud subdomains
+   * @param {string} name - Cookie name.
+   * @param {string} value - Cookie value.
+   * @param {string} days - Number of days when cookie expires.
+   */
+   const setCookie = (name, value, days) => {
+
+      let expires = "";
+      if (days) {
+          let date = new Date();
+          date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+          expires = ";expires=" + date.toUTCString();
+      }
+      document.cookie = name + "=" + (escape(value) || "") + expires + ";path=/;domain=.kenzap.cloud"; 
+  };
+
+  /**
+   * @name getCookie
+   * @description Read cookie by its name.
+   * @param {string} cname - Cookie name.
+   * 
+   * @returns {string} value - Cookie value.
+   */
+  const getCookie = (cname) => {
+
+      let name = cname + "=";
+      let decodedCookie = decodeURIComponent(document.cookie);
+      let ca = decodedCookie.split(';');
+      for (let i = 0; i < ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) == ' ') {
+              c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+              return c.substring(name.length, c.length);
+          }
+      }
+      return "";
+  };
+
+  /**
+   * @name checkHeader
+   * @description This function tracks UI updates, creates header version checksum and compares it after every page reload
+   * @param {object} object - API response.
+   */
+   const checkHeader = () => {
+
+      let version = (localStorage.hasOwnProperty('header') && localStorage.hasOwnProperty('header-version')) ? localStorage.getItem('header-version') : 0;
+      let check = window.location.hostname + '/' + spaceID() + '/' + getCookie('locale');
+      if(check != getCookie('check')){ version = 0; console.log('refresh'); }
+      
+      setCookie('check', check, 5);
+
+      return version
+  };
+
+  /**
+   * @name headers
+   * @description Default headers object for all Kenzap Cloud fetch queries. 
+   * @param {object} headers
+   * @deprecated
+   */
+   ({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + getCookie('kenzap_api_key'),
+      'Kenzap-Locale': getCookie('locale') ? getCookie('locale') : "en",
+      'Kenzap-Header': checkHeader(),
+      'Kenzap-Token': getCookie('kenzap_token'),
+      'Kenzap-Sid': spaceID(),
+  });
+
+  var kMp5sM = _createClass(function kMp5sM(data) {
+    var _this = this;
+    _classCallCheck(this, kMp5sM);
+    _defineProperty(this, "render", function () {
+      var currencyList = ['&#36;', '&#8364;', '&#x20B1;', '&#xa3;', '&#xFFE5', '&#xFFE5;'];
+      var currency = currencyList[_this.data.currency.value];
+      var rates = ['/hr', '/day', '/mo', '/yr'];
+      document.querySelector('#content').insertAdjacentHTML('beforeend', "\n        <section id=\"".concat(attr(_this.data.id), "\" class=\"kMp5sM ").concat(_this.data.c.classes ? attr(_this.data.c.classes) : '', "\" style=\"").concat(attr(_this.data.c.section), "\">\n            <div class=\"container\" style=\"").concat(attr(_this.data.c.container), "\">\n              ").concat(_this.data.header.value, "\n              <div class=\"row\" style=\"--theme-color: ").concat(_this.data.textColor.value, "; --button-color: ").concat(_this.data.buttonColor.value, "\">\n              ").concat(_this.data.items.map(function (item) {
+        return "\n                    <div class=\"col\">\n                      <div class=\"pricing-box\">\n                        <div class=\"img-area\">\n                          <img src=".concat(item.icon.value, " alt=").concat(item.heading.value + ' image', ">\n                        </div>\n                        <div class=\"text-area\">\n                          <h3>").concat(__html(item.heading.value), "</h3>\n                          <p>").concat(__html(item.text.value), "</p>\n                          <h1><sup>").concat(currency, "</sup>").concat(__html(item.price.value), "<sub>").concat(__html(rates[item.rate.value]), "</sub></h1>\n                          <ul>\n                            ").concat(item.features.list.map(function (feature) {
+          return "\n                                <li>".concat(__html(feature.value), "</li>\n                              ");
+        }).join(''), "\n                          </ul>\n                          <a href=").concat(__html(item.buttonLink.value), ">").concat(__html(item.buttonText.value), "</a>\n                        </div>\n                      </div>\n                    </div>\n                  ");
+      }).join(''), "\n              </div>\n          </div>\n        </section>\n        "));
+    });
+    this.data = data;
+    this.render();
+  });
+  window.kMp5sM = kMp5sM;
+
+})();
+//# sourceMappingURL=index.js.map
---
+++ src/pricing-table-1/index.js
@@ -0,0 +1,66 @@
+// dependencies
+import { __html, html, __attr, attr } from '@kenzap/k-cloud';
+
+class kMp5sM{
+
+    // init class
+    constructor(data){
+        
+      // cache data
+      this.data = data;
+    
+      // render section
+      this.render();
+    }
+
+    // render class html
+    render = () => {
+
+      // for currency symbols
+      const currencyList = ['&#36;','&#8364;','&#x20B1;','&#xa3;','&#xFFE5','&#xFFE5;']
+      const currency = currencyList[this.data.currency.value]
+
+      // for rates
+      const rates = ['/hr', '/day', '/mo', '/yr']
+
+      document.querySelector('#content').insertAdjacentHTML('beforeend', 
+        `
+        <section id="${ attr(this.data.id) }" class="kMp5sM ${ this.data.c.classes ? attr(this.data.c.classes) : '' }" style="${ attr(this.data.c.section) }">
+            <div class="container" style="${ attr(this.data.c.container) }">
+              ${ this.data.header.value }
+              <div class="row" style="--theme-color: ${this.data.textColor.value}; --button-color: ${this.data.buttonColor.value}">
+              ${ 
+                this.data.items.map(item => {
+                  return `
+                    <div class="col">
+                      <div class="pricing-box">
+                        <div class="img-area">
+                          <img src=${item.icon.value} alt=${item.heading.value + ' image'}>
+                        </div>
+                        <div class="text-area">
+                          <h3>${ __html(item.heading.value) }</h3>
+                          <p>${ __html(item.text.value) }</p>
+                          <h1><sup>${currency}</sup>${ __html(item.price.value) }<sub>${ __html(rates[item.rate.value]) }</sub></h1>
+                          <ul>
+                            ${item.features.list.map(feature => {
+                              return `
+                                <li>${ __html(feature.value) }</li>
+                              `
+                            }).join('')}
+                          </ul>
+                          <a href=${ __html(item.buttonLink.value) }>${ (__html(item.buttonText.value)) }</a>
+                        </div>
+                      </div>
+                    </div>
+                  `
+                }).join('')           
+              }
+              </div>
+          </div>
+        </section>
+        `
+      );
+    }
+}
+
+window.kMp5sM = kMp5sM;
